eadc67edb19f50810d8c5dd0b3586be8be62f86e
CHUNGTU-120837\Chung Tú <nguyentutsq@gmail.com>
2023-10-06 09:00:56 +0700

    commit Startup - allow connect CORS

--- webApi/BE-V2/BE-V2/Startup.cs
+++ webApi/BE-V2/BE-V2/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -26,7 +26,12 @@ namespace BE_V2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // cho phép kết nối tới client(FE) => Serve(BE)
+            services.AddCors(c => {
+                c.AddPolicy("AcceptToConnectAPI", option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
 
+            services.AddHttpClient();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -37,6 +42,8 @@ namespace BE_V2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //sử dụng kết nối CORS
+            app.UseCors("AcceptToConnectAPI");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
